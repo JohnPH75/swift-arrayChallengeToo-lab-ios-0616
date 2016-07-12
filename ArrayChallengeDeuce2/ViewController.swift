638af3fe3bf0ff4357faa2d92fe57a080945f2d3
JohnPH75 <johnhussain75@gmail.com>
2016-07-12 16:47:21 -0400

    arrayChallengeToo

--- ArrayChallengeDeuce2/ViewController.swift
+++ ArrayChallengeDeuce2/ViewController.swift
@@ -18,10 +18,75 @@ class ViewController: UIViewController {
     }
 
    
+    func addNameToDeliLine(name: String) -> String {
+        
+        switch name {
+            
+        case "Billy Crystal":
+            deliLine.insert(name, atIndex: 0)
+            return "Welcome Billy! You can sit wherever you like."
+            
+        case "Meg Ryan":
+            deliLine.insert(name, atIndex: 0)
+            return "Welcome Meg! You can sit wherever you like."
+            
+        default:
+            deliLine.append(name)
+        }
+        
+        switch deliLine.count {
+            
+        case 1:
+            return "Welcome \(name), you're first in line!"
+            
+        default:
+            return "Welcome \(name), you're number \(deliLine.count) in line."
+        }
+    }
     
+    func nowServing() -> String {
+        
+        var lineGreeting : String = ""
+        
+        if deliLine.count == 0 {
+            
+            lineGreeting = "There is no-one to be served."
+        }
+        else {
+            
+            lineGreeting = "Now serving \(deliLine[0])!"
+        }
+        
+        deliLine.removeAtIndex(0)
+        
+        
+        return lineGreeting
+    }
+    
+    func deliLineDescription() -> String {
+        
+        var lineGreeting2 : String = ""
+        
+        if deliLine.count == 0 {
+            
+            lineGreeting2 = "The line is currently empty."
+        }
+            
+        else {
+            
+            lineGreeting2 = "The line is:\n"
+            
+            for (index, name) in deliLine.enumerate() {
+                
+                lineGreeting2 += "\(index+1) \(name)\n"
+            }
+        }
+        
+            return lineGreeting2
+    }
     
     // Create your methods here
-
+    
 
 }
 
